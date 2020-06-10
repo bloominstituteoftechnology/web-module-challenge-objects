8e2c2fdf89eeb3f930c28a008a4b9591021fea2b
Ava Wingfield <avelinewingfield@gmail.com>
2020-06-10 18:02:35 -0400

    All done but the stretching

--- index.js
+++ index.js
@@ -7,14 +7,18 @@ const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakf
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
 function createMenuItem(name, cost, category){
+
     /* Code here */
+    return {name: name, price: cost, category: category};
 }
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
-
-
-/* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
+let iceCream = createMenuItem("Ice Cream", 5, "Dessert")
+let beans = createMenuItem("Beans", 2, "Side")
+let puffs = createMenuItem("Spinach Puffs", 100, "Appetizer")
+/* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. 
+Add a method to your burger object that automatically calculates price given a string as a parameter. 
 
 Your method should accept: 
 
@@ -23,8 +27,11 @@ Your method should accept:
 and should return a number. 
 
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
-
-
+burger.discount = function(str){
+  if (str === "teacher" || str === "student"){
+    return burger.price * 0.25;
+  }
+  return burger.price * 0.10;}
 
 ///////////////Reviews (MVP)///////////////////
 
@@ -39,12 +46,15 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 ]
 
 /* Task 4: Console.log just Julius' feedback */
-
+console.log(reviews[5].feedback)
 
 /* Task 5: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
+reviews.push( {name: "Ava", rating: 2, feedback: "There was a roach in my food. At least it was cooked?"})
 
 
-/* Task 6: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
+/* Task 6: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"*/
+reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays"
+
 
 /*  Task 7: Write a function to return a review based on the index of the review in the array.
 
@@ -56,12 +66,15 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 and should return a string in the format `{name} gave the restaurant a {rating}, and their feedback was: {feedback}`
  * 
  * For example, if getReviewByIndex is invoked with reviews and the number 0
- * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
+ * it will return `Daniela gave the restaurant a 5 star review and their feedback was: 
+ * Beautiful atmosphere and wonderful vegan options!`
 */
 function getReviewByIndex(reviews, index) {
     /* code here */
+    console.log(`${reviews[index].name} gave the restaurant a ${reviews[index].rating} star review and their feedback was: ${reviews[index].feedback}`)
   }
   
+  getReviewByIndex(reviews, 0)
 
 /* Task 8: Write a function to get information about the most recent review called `getLastReview`
 
@@ -72,9 +85,12 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
+function getLastReview(arr) {
+  return console.log(`${arr[arr.length - 1].name} gave the restaurant a ${arr[arr.length - 1].rating} star rating and their feedback was ${arr[arr.length - 1].feedback}`);
     /* code here */
   } 
+
+  getLastReview(reviews)
 
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
