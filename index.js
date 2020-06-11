3adc7e16f67924c0a433836ca67a48dda266998f
Brit Hemming <hello@brittanyhemming.com>
2020-06-11 17:16:03 -0400

    Update index.js
    updated task numbers - we were missing a task 3

--- index.js
+++ index.js
@@ -38,15 +38,15 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
     {name:"Reyna", rating: 3.5, feedback: ""},
 ]
 
-/* Task 4: Console.log just Julius' feedback */
+/* Task 3: Console.log just Julius' feedback */
 
 
-/* Task 5: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
+/* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
 
 
-/* Task 6: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
+/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
 
-/*  Task 7: Write a function to return a review based on the index of the review in the array.
+/*  Task 6: Write a function to return a review based on the index of the review in the array.
 
  Your function should take two arguments:
 
@@ -63,7 +63,7 @@ function getReviewByIndex(reviews, index) {
   }
   
 
-/* Task 8: Write a function to get information about the most recent review called `getLastReview`
+/* Task 7: Write a function to get information about the most recent review called `getLastReview`
 
 getLastReview should accept:
   (1) an array of objects 
