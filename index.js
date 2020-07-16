43bd607b98ff8be4fa36554f522d34b34a0707c3
Trevor Buchanan <trevor@trevorbuchanan.com>
2020-07-16 13:50:50 -0500

    simplified task 1a

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakf
 
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
-function createMenuItem(name, cost, category){
-    return {name: name, price: cost, category: category}
+function createMenuItem(name, price, category){
+    return {name, price, category}
 }
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
