6466caa05be547de4800706543e4b445d6db3e53
Orlando Castillo <omaddoc@gmail.com>
2020-09-08 17:17:55 -0400

    task 7 completed

--- index.js
+++ index.js
@@ -118,8 +118,8 @@ function getReviewByIndex(reviews, index) {
       return `${reviews[index].name} gave the restaurant a ${reviews[index].rating} star review and their feedback was: ${reviews[index].feedback}`;
   };
 
-console.log(getReviewByIndex(reviews, 0));
-console.log(getReviewByIndex(reviews, 8));
+// console.log(getReviewByIndex(reviews, 0));
+// console.log(getReviewByIndex(reviews, 8));
   
 
 /* Task 7: Write a function to get information about the most recent review called `getLastReview`
@@ -131,10 +131,16 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
+function getLastReview(/* code here */arr) {
     /* code here */
+    let lastIndex = arr.length - 1;
+        return `${arr[lastIndex].name} gave the restaurant a ${arr[lastIndex].rating} star review and their feedback was: ${arr[lastIndex].feedback}`
   } 
+console.log(getLastReview(reviews));
+// console.log(reviews.indexOf(reviews.pop()));
+// console.log(reviews);
 
+// console.log(reviews.length);
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
 
