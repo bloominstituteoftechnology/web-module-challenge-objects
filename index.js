3da1da396766d849fe3941a3edd79b48cf3d55da
ruizaj13 <ruizaj13@gmail.com>
2020-10-08 15:15:31 -0700

    all tasks completed

--- index.js
+++ index.js
@@ -120,7 +120,6 @@ Use the getReviewByIndex function below to do the following:
 
 
 function getReviewByIndex(arr, index) {
-  
   return `${arr[index].name} gave the restaurant a ${arr[index].rating} star review, and their feedback was: ${arr[index].feedback}`;
 }
 
@@ -138,8 +137,8 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(arr) {
+  return  `${arr[arr.length-1].name} gave the restaurant a ${arr[arr.length-1].rating} star review, and their feedback was: ${arr[arr.length-1].feedback}`
 } 
 
 
