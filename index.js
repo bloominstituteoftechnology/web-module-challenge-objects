b9321e68dcbc538cda696470bcda42ad51164b7d
Hairo Garcia <69333686+xpeedy@users.noreply.github.com>
2020-10-08 14:53:15 -0400

    1st-task

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ The function should:
   2. Create and return an object using the received values  
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(name,price,category){
+    return {name,price,category}
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
