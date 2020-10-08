f1998406c79ca06ae3548b63a680b7d175caaf67
Hairo Garcia <69333686+xpeedy@users.noreply.github.com>
2020-10-08 16:56:46 -0400

    task-7-done

--- index.js
+++ index.js
@@ -125,10 +125,11 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(array) {
+  
+  return `${array[array.length - 1].name} gave the restaurant a ${array[array.length - 1].rating} star review, and their feedback was: ${array[array.length - 1].feedback}`;
 } 
-
+console.log(getLastReview(reviews))
 
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
