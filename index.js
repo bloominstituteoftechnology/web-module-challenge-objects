497e5e7ef85ce359f0d57f221aa891b8892d8319
Saeed Khan <saeedkhan@Saeeds-MacBook-Pro.local>
2020-10-25 18:11:57 -0600

    I completed task 4

--- index.js
+++ index.js
@@ -74,22 +74,22 @@ console.log(burger.discount('teacher'));
 
 
 ///////////////Reviews (MVP)///////////////////
-// const reviews = [
-//     {name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and wonderful vegan options!"},
-//     {name: "Jack", rating: 3, feedback:"A little too hipster for my taste, but the burger was decent, if overpriced"},
-//     {name: "Miranda", rating: 4, feedback:"fun trivia and cool vibes"},
-//     {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
-//     {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
-//     {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." },
-//     {name: "Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."},
-//     {name: "Reyna", rating: 3.5, feedback: ""},
-// ]
+const reviews = [
+    {name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and wonderful vegan options!"},
+    {name: "Jack", rating: 3, feedback:"A little too hipster for my taste, but the burger was decent, if overpriced"},
+    {name: "Miranda", rating: 4, feedback:"fun trivia and cool vibes"},
+    {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
+    {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
+    {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." },
+    {name: "Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."},
+    {name: "Reyna", rating: 3.5, feedback: ""},
+]
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Using the reviews array above:
   1. log only Julius' feedback to the console
 */
-
+console.log(reviews[5].feedback);
 
 
 
@@ -98,7 +98,14 @@ Using the reviews array above do the following:
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
-
+  let myReview = 
+  {
+    name: 'Saeed Khan',
+    rating: 4.5,
+    feedback: 'Very welcoming atmosphere. Please try to keep the music low',
+  };
+  reviews.push(myReview);
+  console.log(reviews);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
