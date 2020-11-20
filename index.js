5a3f0d8d45b3771e27200c59601d6a12cef8bb9d
Pape Ndiaye <pape@Papes-MacBook-Pro.local>
2020-11-20 01:36:31 -0500

    MVP

--- index.js
+++ index.js
@@ -143,9 +143,11 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
-} 
+function getLastReview(review,){
+  console.log(`${review[7].name} gave the restaurant a ${review[7].rating} star review, and their feedback was: ${review[7].feedback}`);
+}
+
+getLastReview(reviews);
 
 
 
