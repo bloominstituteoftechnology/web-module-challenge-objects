b4df0fab1586e8ab6d1690150c3d3f8a42eb60c3
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2020-12-10 03:25:38 -0500

    initial

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*MAKE SURE TO RETURN ALL OF THE ANSWERS ON THESE TASKS, IF YOU DON'T, THE AUTOGRADER WILL NOT WORK*/
+/*MAKE SURE TO RETURN ALL OF THE ANSWERS ON THESE TASKS, IF YOU DON'T, THE AUTOGRADER WILL NOT WORK*/ /* */
 
 /*When doing these tasks, we recommend using console.log to test the output of your code to make sure it works correctly.*/
 
