6435b20e5a95fcf19bea4641ceebee33884dab83
Stav Corcos <poonchys@gmail.com>
2021-01-20 13:52:51 -0500

    Finished project involving objects

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(name, price, category){
+    return {name: name, price: price, category:category}
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -48,7 +48,14 @@ export const burger = {
   name: "Burger", 
   price: 18, 
   category: "Lunch", 
-  /*Your code here*/
+  discount: function(string){
+    if (string == "teacher" || string == student){
+      this.price = this.price * .75
+    } else {
+      this.price = this.price * .9
+    }
+    return this.price
+  }
 }
 
 
@@ -69,6 +76,7 @@ const reviews = [
 Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
+console.log(reviews[5].feedback)
 
 
 
@@ -78,7 +86,8 @@ Using the reviews array above do the following: (no function needed)
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
-
+reviews.push({name: "Stav", rating: 3, feedback: "It was alright"})
+console.log(reviews)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -86,6 +95,8 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   1. Add this feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
   2. log the reviews array to the console to check your work
 */
+reviews[reviews.length - 2].feedback = "this place is chill with really cool people, great for getting work done on weekdays"
+console.log(reviews)
 
 
 
@@ -102,8 +113,8 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex(array, number) {
+  return array[number].name + " gave the restaurant a " + array[number].rating + " star review, and their feedback was: " + array[number].feedback
 }
 
 
@@ -121,8 +132,8 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(array) {
+  return getReviewByIndex(array, array.length - 1)
 } 
 
 
@@ -143,8 +154,14 @@ Use the getReviewsByRating function below to do the following:
   ]
 */
 
- function getReviewByRating(/* code here */) {
-    /* code here */
+ function getReviewByRating(array, rating) {
+    let newarr = []
+    for (let i = 0; i < array.length; i++){
+      if (Math.floor(array[i].rating) == rating){
+        newarr.push(array[i])
+      }
+    }
+    return newarr
   }
 
   
@@ -161,8 +178,14 @@ Use the getLongReviews function below to do the following:
   ]
 */
 
-function getLongReviews(/* code here */) {
-    /* code here */
+function getLongReviews(array) {
+    let newarr = []
+    for (let i = 0; i < array.length; i++){
+      if (array[i].feedback.split(" ").length > 15){
+        newarr.push(array[i])
+      }
+    }
+    return newarr
   }
   
 
@@ -184,9 +207,15 @@ Use the carMaker function below to do the following:
 */
 
 
-function carMaker(/* code here */) {
-    /* code here */
-    
+function carMaker(odometer) {
+  const car = {
+    odometer: odometer,
+    drive: function(miles){
+      this.odometer += miles
+      return this.odometer
+    }
+  }
+  return car
 }
 
 
