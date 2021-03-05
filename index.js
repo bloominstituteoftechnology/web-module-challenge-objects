1e4a08bd7276d11dd6399a96e21b6b1a934522f9
Jacob Brooks <myrefugemyfortress@yahoo.com>
2021-03-05 00:50:04 -0500

    WOrking on it

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(){
+   WORKING ON IT
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
