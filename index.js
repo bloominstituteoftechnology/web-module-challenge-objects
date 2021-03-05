65bc72c4b005b148d0010fcfed24762bb2eef480
Owen  Damron <damronowen@yahoo.com>
2021-03-05 00:12:32 -0600

    updated

--- index.js
+++ index.js
@@ -15,8 +15,35 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(name, price, category){
+  return {
+    name,
+    price,
+    category
+  }
+}
+
+const menuItems = [
+{
+name: 'Cafe Latte',
+price: 4,
+category: 'Drinks'
+},
+{
+name: 'Breakfast Burrito',
+price: 16,
+category: 'Breakfast'
+},
+{
+name: 'Tacos',
+price: 8,
+category: 'Lunch'
+},
+]
+
+for(let i = 0; i < menuItems.length; i++) {
+console.log(menuItems[i])
+console.log(menuItems[i].name)
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -28,7 +55,36 @@ Test your createMenuItems function by doing the following:
   
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
+function createMenuItem(name, price, category){
+  return {
+    name,
+    price,
+    category
+  }
+}
 
+const newMenuItems = [
+{
+name: 'Croissant',
+price: 6,
+category: 'Pastry'
+},
+{
+name: 'Tapas',
+price: 20,
+category: 'Dinner'
+},
+{
+name: 'Al Carbon Tacos',
+price: 17,
+category: 'Lunch'
+},
+]
+
+for(let i = 0; i < newMenuItems.length; i++) {
+console.log(newMenuItems[i])
+console.log(newMenuItems[i].name)
+}
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -69,7 +125,8 @@ const reviews = [
 Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
-
+console.log(reviews[5])
+console.log(reviews[5].name)
 
 
 
@@ -78,7 +135,8 @@ Using the reviews array above do the following: (no function needed)
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
-
+reviews.push({name: "Owen", rating: 4.5, feedback: "Awesome place you got here."})
+console.log(reviews)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -102,10 +160,16 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex(name, rating, feedback) {
+  return {
+    name,
+    rating,
+    feedback
+  }
 }
 
+console.log(reviews[4]);
+console.log(reviews[4].name);
 
   
 
