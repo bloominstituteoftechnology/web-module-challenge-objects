f247dc302dfe0f02c94420f78d3fb98dc3865327
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-03-06 14:09:20 -0600

    moving on to task 6

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ export const burger = {
       burger.price = burger.price - burger.price * .25;
     } else if(type === 'student'){
       burger.price = burger.price - burger.price * .25;
-    } else if(type === 'public'){
+    } else{
       burger.price = burger.price - burger.price * .10;
     }
     return burger.price
@@ -88,7 +88,7 @@ Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
 
-
+console.log(reviews[5].feedback)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -96,17 +96,23 @@ Using the reviews array above do the following: (no function needed)
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
+reviews.push({
+  'name': 'Steve',
+  'rating': 5,
+  'feedback': "Food was great"
+})
 
 
+console.log(reviews)
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Reyna's feedback is missing! Use what you know to do the following: (no function needed) 
   1. Add this feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
   2. log the reviews array to the console to check your work
 */
+reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays"
 
-
-
+console.log(reviews[7])
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -120,8 +126,8 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex() {
+  
 }
 
 
