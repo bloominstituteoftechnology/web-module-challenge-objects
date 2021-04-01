c970967373f521fb38a28fb598b4bab620685213
Amlak befekadu <amlakbarkon@gmail.com>
2021-04-01 10:28:18 -0400

    thanks Joson

--- index.js
+++ index.js
@@ -197,12 +197,20 @@ Use the carMaker function below to do the following:
   It would return 110 because it was created with 10 as the odometer and we added 100 to it with the drive method 
 */
 
+function carMaker(miles) { 
+   const carObj = {    
+     odometer: miles,
+    drive: function(distance){ 
+           this.odometer += distance;
+          return this.odometer;
+            } 
+        } 
+         return carObj
+     }
+        
+        let car1 = carMaker(10);
 
-function carMaker(/* code here */) {
-    /* code here */
-    
-}
-
+        console.log(car1.drive(100));
 
 /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
 function foo(){
