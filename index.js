84eb3b499c2671c763134d8cd7541e94a0345509
Sayo <sl.deve080412@gmail.com>
2021-04-15 12:03:32 -0700

    starting today's project

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*MAKE SURE TO RETURN ALL OF THE ANSWERS ON THESE TASKS, IF YOU DON'T, THE AUTOGRADER WILL NOT WORK*/
-
+start
 /*When doing these tasks, we recommend using console.log to test the output of your code to make sure it works correctly.*/
 
 ///////////////Menu Items (MVP)///////////////////
