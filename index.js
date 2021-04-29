d118a993a42c81b0d9f0ec7e51fa2f8b369599b0
Diego <arancibiadiego97@gmail.com>
2021-04-28 21:23:43 -0400

    Initial Commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*MAKE SURE TO RETURN ALL OF THE ANSWERS ON THESE TASKS, IF YOU DON'T, THE AUTOGRADER WILL NOT WORK*/
 
 /*When doing these tasks, we recommend using console.log to test the output of your code to make sure it works correctly.*/
-
+ 
 ///////////////Menu Items (MVP)///////////////////
 const latte = {name: "Cafe Latte", price: 4, category: "Drinks"};
 const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakfast"};
