8c2f74175cc6ce2323ce5f992e339a4efff8032b
Gtorres21 <giovannytorres21@gmail.com>
2021-04-29 19:06:46 -0700

    Completed all MVP Task

--- index.js
+++ index.js
@@ -142,10 +142,10 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(arr) {
+  return `${arr[arr.length-1].name} gave the restaurant a ${arr[arr.length-1].rating} star review, and their feedback was: ${arr[arr.length-1].feedback}`;
 } 
-
+console.log(getLastReview(reviews));
 
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
