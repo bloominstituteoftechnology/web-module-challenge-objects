d60a411dcab0233b3cda3cc7529b7580ca7b9e4b
xavier <xaviervickwest@gmail.com>
2021-06-10 15:58:48 -0400

    done with task 5

--- index.js
+++ index.js
@@ -91,8 +91,9 @@ Using the reviews array above do the following: (no function needed)
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
-reviews.push({name: "Xavier", rating: 5, feedback: "Best place ever"})
+reviews.push({name: "Xavier", rating: 5, feedback: "Best place ever"});
 console.log(reviews)
+
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -100,7 +101,8 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   1. Add this feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
   2. log the reviews array to the console to check your work
 */
-
+reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays";
+console.log(reviews[7])
 
 
 
