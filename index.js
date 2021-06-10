f64f60a583f3fda0d6e44f23a9c8587e1571bfd5
xavier <xaviervickwest@gmail.com>
2021-06-10 15:25:21 -0400

    done with task 1a

--- index.js
+++ index.js
@@ -15,10 +15,14 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(name, price, category){
+    let obj = {}
+    obj.name = name;
+    obj.price = price;
+    obj.category = category;
+    return obj;
 }
-
+createMenuItem('tacos', 8, 'Lunch'); 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Invoke your function!
 Test your createMenuItems function by doing the following:
