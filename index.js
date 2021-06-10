7c950e95d6969b16e38a51757af183802dea65d4
Andrew Cummings <andrew.amalee@gmail.com>
2021-06-10 13:17:11 -0700

    testing

--- index.js
+++ index.js
@@ -36,11 +36,6 @@ Test your createMenuItems function by doing the following:
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
 
-console.log(createMenuItem('Pizza', 5, 'Dinner'));
-
-// console.log(food);
-
-
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to the 
