8a83eab188ddb1b6c6be72ab115f4f55c7602f60
Kade Griffith <kadeg95@gmail.com>
2021-07-15 13:24:05 -0600

    put new codgrade code into codegrade_mvp.test.js

--- codegrade_mvp.test.js
+++ codegrade_mvp.test.js
@@ -1,7 +1,8 @@
 const functions  = require('./index.js');
 const { burger } = require('./index.js');
-
-const reviews = [
+let reviews;
+beforeEach(()=>{
+reviews = [
     {name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and wonderful vegan options!"},
     {name: "Jack", rating: 3, feedback:"A little too hipster for my taste, but the burger was decent, if overpriced"},
     {name: "Miranda", rating: 4, feedback:"fun trivia and cool vibes"},
@@ -11,19 +12,17 @@ const reviews = [
     {name:"Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."},
     {name:"Reyna", rating: 3.5, feedback: "this place is chill with really cool people, great for getting work done on weekdays"},
 ]
-
+})
 describe('fooFunction', ()=>{
     it('foo returns foo', ()=>{
         expect(functions.foo()).toBe('bar');
     })
 });
-
 describe('createMenuItem', ()=>{
     it('createMenuItem returns object', ()=>{
         expect(functions.createMenuItem('taco', 8, 'lunch')).toEqual({name: 'taco', price: 8, category:'lunch'});
     })
 });
-
 // test burger method discount here 
 describe('burger.discount', ()=>{
     it('discount for teacher', ()=>{
@@ -36,7 +35,6 @@ describe('burger.discount', ()=>{
         expect(burger.discount('public')).toBe(16.2);
     })
 });
-
 describe('addReview', ()=>{
     it('addReview returns an array of objects', ()=>{
         expect(functions.addReview(reviews, 'Daniela', 5, 'great eats!')).toEqual([
@@ -79,50 +77,7 @@ describe('addReview', ()=>{
             { name: 'Daniela', rating: 5, feedback: 'great eats!' }
           ]);
     })
-    it('addReview returns an array of objects', ()=>{
-        expect(functions.addReview(reviews, 'George', 2, 'not so good')).toEqual([
-            {
-              name: 'Daniela',
-              rating: 5,
-              feedback: 'Beautiful atmosphere and wonderful vegan options!'
-            },
-            {
-              name: 'Jack',
-              rating: 3,
-              feedback: 'A little too hipster for my taste, but the burger was decent, if overpriced'
-            },
-            { name: 'Miranda', rating: 4, feedback: 'fun trivia and cool vibes' },
-            {
-              name: 'Wen',
-              rating: 4.5,
-              feedback: "I don't leave my house often, but when I do, it's for this place. Highly reccomend."
-            },
-            {
-              name: 'Brett',
-              rating: 3,
-              feedback: 'great selection of snacks and a nice cafe area to get work done during the day.'
-            },
-            {
-              name: 'Julius',
-              rating: 2,
-              feedback: 'I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it.'
-            },
-            {
-              name: 'Lauren',
-              rating: 4,
-              feedback: 'Absolutely love that they have karaoke Fridays! Food and drink selection is okay.'
-            },
-            {
-              name: 'Reyna',
-              rating: 3.5,
-              feedback: 'this place is chill with really cool people, great for getting work done on weekdays'
-            },
-            { name: 'Daniela', rating: 5, feedback: 'great eats!' },
-            { name: 'George', rating: 2, feedback: 'not so good' }
-          ]);
-    })
 });
-
 describe('getReviewByIndex', ()=>{
     it('getReviewByIndex returns a string', ()=>{
         expect(functions.getReviewByIndex(reviews, 0)).toBe("Daniela gave the restaurant a 5 star review, and their feedback was: Beautiful atmosphere and wonderful vegan options!");
@@ -131,10 +86,8 @@ describe('getReviewByIndex', ()=>{
         expect(functions.getReviewByIndex(reviews, 2)).toBe("Miranda gave the restaurant a 4 star review, and their feedback was: fun trivia and cool vibes");
     })
 });
-
 describe('getLastReview', ()=>{
     it('getLastReview returns a string', ()=>{
         expect(functions.getLastReview(reviews)).toBe(`Reyna gave the restaurant a 3.5 star review, and their feedback was: this place is chill with really cool people, great for getting work done on weekdays`);
     })
 });
-
