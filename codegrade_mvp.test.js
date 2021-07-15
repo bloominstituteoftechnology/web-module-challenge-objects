36d42f70a0b8d07501fb95b6b6c1fa7674e66860
Brit Hemming <hello@brittanyhemming.com>
2021-07-15 14:08:41 -0400

    Update codegrade_mvp.test.js

--- codegrade_mvp.test.js
+++ codegrade_mvp.test.js
@@ -1,7 +1,9 @@
 const functions  = require('./index.js');
 const { burger } = require('./index.js');
 
-const reviews = [
+let reviews;
+beforeEach(()=>{
+reviews = [
     {name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and wonderful vegan options!"},
     {name: "Jack", rating: 3, feedback:"A little too hipster for my taste, but the burger was decent, if overpriced"},
     {name: "Miranda", rating: 4, feedback:"fun trivia and cool vibes"},
@@ -11,6 +13,7 @@ const reviews = [
     {name:"Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."},
     {name:"Reyna", rating: 3.5, feedback: "this place is chill with really cool people, great for getting work done on weekdays"},
 ]
+})
 
 describe('fooFunction', ()=>{
     it('foo returns foo', ()=>{
@@ -77,48 +80,6 @@ describe('addReview', ()=>{
               feedback: 'this place is chill with really cool people, great for getting work done on weekdays'
             },
             { name: 'Daniela', rating: 5, feedback: 'great eats!' }
-          ]);
-    })
-    it('addReview returns an array of objects', ()=>{
-        expect(functions.addReview(reviews, 'George', 2, 'not so good')).toEqual([
-            {
-              name: 'Daniela',
-              rating: 5,
-              feedback: 'Beautiful atmosphere and wonderful vegan options!'
-            },
-            {
-              name: 'Jack',
-              rating: 3,
-              feedback: 'A little too hipster for my taste, but the burger was decent, if overpriced'
-            },
-            { name: 'Miranda', rating: 4, feedback: 'fun trivia and cool vibes' },
-            {
-              name: 'Wen',
-              rating: 4.5,
-              feedback: "I don't leave my house often, but when I do, it's for this place. Highly reccomend."
-            },
-            {
-              name: 'Brett',
-              rating: 3,
-              feedback: 'great selection of snacks and a nice cafe area to get work done during the day.'
-            },
-            {
-              name: 'Julius',
-              rating: 2,
-              feedback: 'I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it.'
-            },
-            {
-              name: 'Lauren',
-              rating: 4,
-              feedback: 'Absolutely love that they have karaoke Fridays! Food and drink selection is okay.'
-            },
-            {
-              name: 'Reyna',
-              rating: 3.5,
-              feedback: 'this place is chill with really cool people, great for getting work done on weekdays'
-            },
-            { name: 'Daniela', rating: 5, feedback: 'great eats!' },
-            { name: 'George', rating: 2, feedback: 'not so good' }
           ]);
     })
 });
