55ce2e4c816631fb6f20a5711224c5d6f5369545
beatlesm <rj.geng@gmail.com>
2021-09-09 11:27:31 -0700

    web48 1.2 Module 4 test Codegrad

--- index.js
+++ index.js
@@ -15,9 +15,13 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
+function createMenuItem(name, price, category){
     /*Your code here*/
+    return {name, price, category};
 }
+
+console.log(createMenuItem('tacos', 8, 'Lunch'));
+
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Invoke your function!
